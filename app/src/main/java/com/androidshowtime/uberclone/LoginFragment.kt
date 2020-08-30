4d4d2b500820e1bcf7b8ded91f9ac6bfeec64511
Antony <vontonnie@gmail.com>
2020-08-30 20:48:35 +0300

    hide action bar on login fragment

--- app/src/main/java/com/androidshowtime/uberclone/LoginFragment.kt
+++ app/src/main/java/com/androidshowtime/uberclone/LoginFragment.kt
@@ -1,28 +1,31 @@
 package com.androidshowtime.uberclone
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.androidshowtime.uberclone.databinding.FragmentLoginBinding
 
 
 class LoginFragment : Fragment() {
 
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
 
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
+        val binding = FragmentLoginBinding.inflate(inflater)
 
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+
+
+
+        (activity as AppCompatActivity).supportActionBar?.hide()
+        return binding.root
     }
 
 
