eadca7e5a0b6b6e9a1172c568f3feee644243bc9
Antony <vontonnie@gmail.com>
2020-08-30 21:14:15 +0300

    Anonymous sign in enabled

--- app/src/main/java/com/androidshowtime/uberclone/LoginFragment.kt
+++ app/src/main/java/com/androidshowtime/uberclone/LoginFragment.kt
@@ -4,12 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.androidshowtime.uberclone.databinding.FragmentLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import timber.log.Timber
 
 
 class LoginFragment : Fragment() {
+
+    private lateinit var auth: FirebaseAuth
+
 
 
 
@@ -20,12 +26,38 @@ class LoginFragment : Fragment() {
     ): View? {
 
         val binding = FragmentLoginBinding.inflate(inflater)
-
-
+        auth = FirebaseAuth.getInstance()
+        signInAnonymously()
 
 
         (activity as AppCompatActivity).supportActionBar?.hide()
+
+
+        Timber.i("Switch Value:, ${binding.switch1.isChecked}")
         return binding.root
+    }
+
+
+    private fun signInAnonymously() {
+//call signInAnonymously to sign in as an anonymous user
+        auth.signInAnonymously().addOnCompleteListener {
+
+
+            if (it.isSuccessful) {
+
+                Toast.makeText(activity, "Anonymous Login Successful", Toast.LENGTH_SHORT).show()
+                Timber.i("Anonymous Login Successful")
+
+            } else {
+
+                Toast.makeText(activity, "Anonymous Login Failed", Toast.LENGTH_SHORT).show()
+                Timber.i("Anonymous Login Failed")
+
+            }
+
+
+        }
+
     }
 
 
