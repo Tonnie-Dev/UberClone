0a5919782003654a9010f5439968cbfe9df36294
Antony <vontonnie@gmail.com>
2020-08-30 18:06:59 +0300

    initial commit

---
+++ app/src/main/java/com/androidshowtime/uberclone/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.androidshowtime.uberclone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
