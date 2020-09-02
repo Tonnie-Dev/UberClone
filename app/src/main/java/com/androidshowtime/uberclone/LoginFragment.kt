f591c87034f148a5698105aba3f6365171b2d008
Antony <vontonnie@gmail.com>
2020-09-02 18:47:59 +0300

    Add Places Library

--- app/src/main/java/com/androidshowtime/uberclone/LoginFragment.kt
+++ app/src/main/java/com/androidshowtime/uberclone/LoginFragment.kt
@@ -1,12 +1,14 @@
 package com.androidshowtime.uberclone
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.androidshowtime.uberclone.databinding.FragmentLoginBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
@@ -24,15 +26,27 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        val binding = FragmentLoginBinding.inflate(inflater)
         auth = FirebaseAuth.getInstance()
-        db = FirebaseDatabase.getInstance()
         signInAnonymously()
+//obtain reference to Database
+
+        val binding = FragmentLoginBinding.inflate(inflater)
+
+        db = FirebaseDatabase.getInstance()
+
 
 //hide actionBar
         (activity as AppCompatActivity).supportActionBar?.hide()
 
+        val currentUser = auth.currentUser
 
+        if (currentUser != null) {
+            Timber.i("the user is ${currentUser.uid}")
+        }
+        else{
+
+            Timber.i("user is null null")
+        }
         //button onClickListener
         binding.loginButton.setOnClickListener {
             //set the user type as rider by default
@@ -41,18 +55,27 @@ class LoginFragment : Fragment() {
 
                 userType = "Driver"
             }
-//obtain reference to Database
-            val currentUser = auth.currentUser!!
+
 
 
             val userMap = mapOf("User Type" to userType)
-            db.reference.child("Users").child(currentUser.uid).setValue(userMap)
+            if (currentUser != null) {
+                db.reference.child("Users").child(currentUser.uid).setValue(userMap)
+            }
 
 
             when(userType){
 
-                "Rider" -> {}
-                "Driver" -> {}
+                "Rider" -> {
+
+
+                    findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToRiderFragment()) }
+                "Driver" -> {
+
+
+                    findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToDriverFragment())
+
+                }
 
 
 
--- app/src/main/java/com/androidshowtime/uberclone/RiderFragment.kt
+++ app/src/main/java/com/androidshowtime/uberclone/RiderFragment.kt
@@ -8,7 +8,6 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
@@ -36,7 +35,7 @@ class RiderFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_maps, container, false)
+        return inflater.inflate(R.layout.fragment_rider, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
