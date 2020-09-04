06b946a58750233f3629ea54c388a48113483998
Antony <vontonnie@gmail.com>
2020-09-04 08:02:54 +0300

    Add Locations Updates

--- app/src/main/java/com/androidshowtime/uberclone/RiderFragment.kt
+++ app/src/main/java/com/androidshowtime/uberclone/RiderFragment.kt
@@ -1,12 +1,12 @@
 package com.androidshowtime.uberclone
 
 import android.annotation.SuppressLint
+import android.location.Location
 import android.os.Bundle
 import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import com.google.android.gms.location.*
@@ -18,18 +18,66 @@ import com.google.android.gms.maps.model.MarkerOptions
 import timber.log.Timber
 
 class RiderFragment : Fragment() {
-private lateinit var lastKnownLocation:LatLng
+    private lateinit var lastKnownLocation: LatLng
+    private lateinit var currentLocation: Location
+
+    // Single Permission Contract
+    @SuppressLint("MissingPermission")
+    val reqPerm = registerForActivityResult(ActivityResultContracts.RequestPermission()) { result ->
+
+        if (result) {
+
+            Timber.i("Location Permission Granted")
+            startLocationUpdates()
+
+        }
+        else {
+
+
+            Timber.i("Location Permission Denied")
+        }
+
+
+    }
+
 
     //components for locations request
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     private lateinit var locationRequest: LocationRequest
-    private lateinit var locationCallback: LocationCallback
-    private val callback = OnMapReadyCallback { googleMap ->
 
+    //initialize locationCallback to get notification
+    private val locationCallback = object : LocationCallback() {
+
+        //Called when the device location is available.
+        override fun onLocationResult(locationResult: LocationResult?) {
+            super.onLocationResult(locationResult)
+            Timber.i("onLocationResult Triggered")
+
+            if (locationResult != null) {
+                //locationResult.locations.forEach { currentLocation = it }
+                currentLocation = locationResult.lastLocation
+                Timber.i(
+                    "Current Place:  ${currentLocation.latitude}, ${currentLocation.longitude}")
+            }
+            else {
+
+                Timber.i("Location is null!!!")
+                return
+
+            }
+        }
+
+
+    }
+
+
+    private val callback = OnMapReadyCallback { googleMap ->
         val sydney = LatLng(-34.0, 151.0)
         googleMap.addMarker(MarkerOptions().position(sydney)
                                     .title("Marker in Sydney"))
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+
     }
 
     override fun onCreateView(
@@ -43,35 +91,15 @@ private lateinit var lastKnownLocation:LatLng
         //initializing the fusedLocationProviderClient
         fusedLocationProviderClient =
                 LocationServices.getFusedLocationProviderClient(requireActivity())
+        locationRequest = LocationRequest().apply {
 
 
-        //creating LocationRequest
-        locationRequest = LocationRequest.create()
-                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
-                .setInterval(5000)
-                .setFastestInterval(3000)
-
-
-        //initialize locationCallback to get notification
-        locationCallback = object : LocationCallback() {
-
-            override fun onLocationResult(p0: LocationResult?) {
-                super.onLocationResult(p0)
-            }
-
-            override fun onLocationAvailability(p0: LocationAvailability?) {
-                super.onLocationAvailability(p0)
-            }
-
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+            interval = 3000
+            fastestInterval = 5000
         }
 
-
-
-
-
-
-
-
+        
         return inflater.inflate(R.layout.fragment_rider, container, false)
     }
 
@@ -82,35 +110,8 @@ private lateinit var lastKnownLocation:LatLng
 
     }
 
-    @SuppressLint("MissingPermission")
-    fun startLocationUpdates() {
 
-
-    }
-    // Single Permission Contract
-
-    @SuppressLint("MissingPermission") val reqPerm =
-            registerForActivityResult(ActivityResultContracts.RequestPermission()) { resut ->
-
-                if (resut) {
-
-                    Timber.i("Location Permission Granted")
-
-                    fusedLocationProviderClient.requestLocationUpdates(locationRequest,
-                                                                       locationCallback,
-                                                                       Looper.getMainLooper())
-                }
-                else {
-
-
-                    Timber.i("Location Permission Denied")
-                }
-
-
-            }
     //request permission
-
-
     @SuppressLint("MissingPermission")
     fun getLastKnownLocation(): LatLng {
 
@@ -124,7 +125,7 @@ private lateinit var lastKnownLocation:LatLng
                     //retrieving values from Location
                     val myLat = it.latitude
                     val myLng = it.longitude
-                   lastKnownLocation = LatLng(myLat, myLng)
+                    lastKnownLocation = LatLng(myLat, myLng)
                 }
             }
 
@@ -136,5 +137,21 @@ private lateinit var lastKnownLocation:LatLng
         return lastKnownLocation
 
     }
+
+
+    //start Location Updates
+
+
+    fun startLocationUpdates() {
+        try {
+            fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback,
+                                                               Looper.getMainLooper())
+        }
+        catch (e: SecurityException) {
+            //Create a function to request necessary permissions from the app.
+
+        }
+    }
+
 
 }
