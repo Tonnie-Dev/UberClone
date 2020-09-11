2e82f42a15a6968a1d076c4fc5bc908f76ece01a
Antony <vontonnie@gmail.com>
2020-09-11 20:02:13 +0300

    Clean-Up Driver Fragment

--- app/src/main/java/com/androidshowtime/uberclone/driver/DriverFragment.kt
+++ app/src/main/java/com/androidshowtime/uberclone/driver/DriverFragment.kt
@@ -1,75 +1,22 @@
 package com.androidshowtime.uberclone.driver
 
+
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import com.androidshowtime.uberclone.R
 
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationCallback
-import com.google.android.gms.location.LocationRequest
-import com.google.android.gms.location.LocationResult
-
-
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
-import timber.log.Timber
-
 class DriverFragment : Fragment() {
-
-    
-
-    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
-    private lateinit var locationRequest: LocationRequest
-
-    private val locationCallback = object : LocationCallback(){
-        override fun onLocationResult(locationResult: LocationResult?) {
-            super.onLocationResult(locationResult)
-
-            if (locationResult != null) {
-
-            }
-        }
-
-    }
-    private val callback = OnMapReadyCallback { googleMap ->
-
-        val sydney = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-    }
-    // Single Permission Contract
-    private val reqPerm = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
-
-        if (it){
-
-            Timber.i("Location Permission Granted - Driver")
-
-        }else{
-            Timber.i("Location Permission Denied - Driver")}
-
-    }
-
 
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+                             ): View? {
         return inflater.inflate(R.layout.fragment_driver, container, false)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
-        mapFragment?.getMapAsync(callback)
-    }
 }
