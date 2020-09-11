51bd30ac248116ff2e949f7d1766d3ad802eab4e
Antony <vontonnie@gmail.com>
2020-09-11 20:12:23 +0300

    Set title to actionBar

--- app/src/main/java/com/androidshowtime/uberclone/driver/DriverFragment.kt
+++ app/src/main/java/com/androidshowtime/uberclone/driver/DriverFragment.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.androidshowtime.uberclone.databinding.FragmentDriverBinding
 
@@ -12,15 +14,23 @@ class DriverFragment : Fragment() {
 
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
                              ): View? {
 
 
+        val binding = FragmentDriverBinding.inflate(inflater)
+
+        (activity as AppCompatActivity).supportActionBar?.show()
+        (activity as AppCompatActivity).supportActionBar?.title = "Nearby Requests"
 
 
-        val binding =FragmentDriverBinding.inflate(inflater)
+        val list = mutableListOf<String>("Sue", "Winnie", "Lucy")
+
+        val adapter = ArrayAdapter<String>(requireActivity(), android.R.layout
+            .simple_list_item_1,list)
+
+        binding.listView.adapter = adapter
+
         return binding.root
     }
 
