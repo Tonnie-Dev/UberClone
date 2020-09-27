0c8bfe78f4e7c301125b62b5819c13b412d1a3ef
Antony <vontonnie@gmail.com>
2020-09-27 09:43:58 +0300

    Add Location Updates to DriverMapFragment

--- app/src/main/java/com/androidshowtime/uberclone/driver/DriverMapFragment.kt
+++ app/src/main/java/com/androidshowtime/uberclone/driver/DriverMapFragment.kt
@@ -9,21 +9,24 @@ import android.graphics.Color
 import android.location.Location
 import android.net.Uri
 import android.os.Bundle
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.androidshowtime.uberclone.R
 import com.androidshowtime.uberclone.databinding.FragmentDriverMapBinding
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.*
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.GeoPoint
 import timber.log.Timber
 
 
@@ -42,12 +45,57 @@ class DriverMapFragment : Fragment() {
 
     //location components
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
-    private lateinit var locationRequest:LocationRequest
+    private lateinit var locationRequest: LocationRequest
+    private var locationCallback = object : LocationCallback() {
+        //override onLocationResult and set it to driver's current location
+        override fun onLocationResult(locationResult: LocationResult?) {
+            super.onLocationResult(locationResult)
+
+            if (locationResult != null) {
+                driverLocation = locationResult.lastLocation
+
+                //obtain Geopoint from driver's location
+                val driverGeoPoint = GeoPoint(driverLocation.latitude, driverLocation.longitude)
+
+
+                //update location on firestore
+                val driverRef = firestore.collection("Driver").document(driverDocId)
+
+                driverRef.update("geoPoint", driverGeoPoint)
+                        .addOnSuccessListener { Timber.i("Driver's location updated on firestore") }
+                        .addOnFailureListener { Timber.i("Error $it") }
+
+            }
+
+        }
+
+
+    }
+
+    //if permission is granted start updates otherwise notify user
+    private val locationPermission =
+            registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+
+                if (it) {
+
+                    startLocationUpdates()
+                }
+                else {
+                    //notify user via Toast
+                    Toast.makeText(activity, "Location Permission Needed", Toast.LENGTH_SHORT)
+                            .show()
+                }
+
+            }
 
 
     private val callback = OnMapReadyCallback { googleMap ->
 
         map = googleMap
+
+        //request for permission
+        locationPermission.launch(android.Manifest.permission.ACCESS_FINE_LOCATION)
+
 
         //retrieve driver and rider location from navigation arguments
         riderLocation = args.userLocation
@@ -67,18 +115,13 @@ class DriverMapFragment : Fragment() {
                 MarkerOptions().position(riderLatLng)
                         .title("Rider")
                         .flat(true)
-                        .snippet(snippet)
-
                         .icon(BitmapDescriptorFactory.fromBitmap(bitmap))
                                        )
 
-        //show the infoWindow permanently
+        //show the infoWindow persistently on the icon
         riderMarker.showInfoWindow()
-        Timber.i("${args.riderDistanceFromDriver} KM Away")
-        markers.add(riderMarker)
 
-        // .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory
-        // .HUE_RED))))
+        markers.add(riderMarker)
 
 
         markers.add(
@@ -86,9 +129,7 @@ class DriverMapFragment : Fragment() {
                         MarkerOptions().position(driverLatLng)
                                 .title("Driver")
                                 .snippet("Driver")
-                                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
-                             )
-                   )
+                                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))))
 
         //create builder
         val builder = LatLngBounds.builder()
@@ -106,12 +147,11 @@ class DriverMapFragment : Fragment() {
 
 
         //move and animate the camera
-
         map.moveCamera(cu)
         map.animateCamera(CameraUpdateFactory.zoomTo(11f), 2000, null)
 
 
-        //draw polygon
+        //draw a thin polygon
         val latLngList = mutableListOf(riderLatLng, driverLatLng)
         showPath(latLngList)
     }
@@ -124,11 +164,23 @@ class DriverMapFragment : Fragment() {
 
         //create binding
         val binding = FragmentDriverMapBinding.inflate(inflater)
+
+
         //retrieve docID
         val userDocId = args.userDocId
         //initialize markers list
         markers = mutableListOf()
 
+
+        //initialize location components
+        fusedLocationProviderClient =
+                LocationServices.getFusedLocationProviderClient(requireActivity())
+        locationRequest = LocationRequest().apply {
+
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+            interval = 5000
+            fastestInterval = 3000
+        }
 
         //acceptButton onClick implementation
         binding.acceptButton.setOnClickListener {
@@ -136,7 +188,7 @@ class DriverMapFragment : Fragment() {
             firestore = FirebaseFirestore.getInstance()
 
 
-            //update the requestAccepted field value in firestore
+            //update rider's requestAccepted field value in firestore
             val userRef = firestore
                     .collection("UberRequest")
                     .document(userDocId)
@@ -144,20 +196,14 @@ class DriverMapFragment : Fragment() {
 
             userRef.update("accepted", true).addOnSuccessListener {
 
-                Timber.i("update done - $userDocId")
+                Timber.i("rider's request Accepted")
 
-                /*val driverDoc = args.driverDocId
-                val userId = args.userDocId
-
-                val driverRef = firestore.collection("Driver").document(driverDoc)
-
-                driverRef.update("driverActivationId",userDocId )*/
             }.addOnFailureListener {
 
-                Timber.i("update Failed!!")
+                Timber.i("Rider's request acceptance failed - $it")
             }
 
-            //Tie driver to the request
+            //Tie driver to the rider's request
             driverDocId = args.driverDocId
 
             val driverRef = firestore
@@ -222,6 +268,23 @@ class DriverMapFragment : Fragment() {
         magentaPolyline = map.addPolyline(polylineOptions)
 
 
+    }
+
+
+    fun startLocationUpdates() {
+
+
+        try {
+
+            //request for Location Updates
+            fusedLocationProviderClient.requestLocationUpdates(
+                    locationRequest, locationCallback,
+                    Looper.getMainLooper())
+        }
+        catch (e: SecurityException) {
+
+
+        }
     }
 
 
